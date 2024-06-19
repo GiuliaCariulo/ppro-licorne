ee48280e466dde5c70d17e3c580311e4372ce18e
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2024-06-19 19:46:25 +0200

    je vais y arriver un jour

---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+// let currentLeftPercent = 0;
+
+// document
+//   .querySelector(".swiper-arrow-right")
+//   .addEventListener("click", function () {
+//     const element = document.querySelector(".swiper-bar");
+//     currentLeftPercent -= 100;
+//     element.style.left = currentLeftPercent + "%";
+//   });
+
+// document
+//   .querySelector(".swiper-arrow-left")
+//   .addEventListener("click", function () {
+//     const element = document.querySelector(".swiper-bar");
+//     currentLeftPercent -= -100;
+//     element.style.left = currentLeftPercent + "%";
+//   });
+{
+  /* <div class="swiper">
+        <div class="swiper-bar">
+          <div class="swiper-slide swiper-slide-1">slide-1</div>
+          <div class="swiper-slide swiper-slide-2">slide-2</div>
+          <div class="swiper-slide swiper-slide-3">slide-3</div>
+        </div>
+      </div>
+      <div class="swiper-arrow swiper-arrow-left">0</div>
+      <div class="swiper-arrow swiper-arrow-right">0</div> */
+}
+// .swiper {
+// }
+
+// .swiper-bar {
+//   position: relative;
+//   left: 0;
+//   transition: left 0.5s;
+// }
+
+// .swiper-arrow {
+//   padding: 20px;
+//   cursor: pointer;
+//   background-color: $yellow;
+// }
+
+// .swiper-arrow-right {
+//   text-align: right;
+// }
+
+// .swiper-slide {
+//   position: relative;
+//   width: 100%;
+//   height: 150px;
+//   padding: 50px 0;
+//   text-align: center;
+// }
+
+// .swiper-slide-1 {
+//   background-color: rgb(238, 161, 255);
+// }
+
+// .swiper-slide-2 {
+//   left: 100%;
+//   bottom: 150px;
+//   background-color: rgb(161, 255, 166);
+// }
+
+// .swiper-slide-3 {
+//   left: 200%;
+//   bottom: 300px;
+//   background-color: rgb(161, 203, 255);
+// }
